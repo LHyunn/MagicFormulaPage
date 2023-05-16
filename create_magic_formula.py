f243a17f7c7bf9f31a9bd50445bbcde87e683e92
LHyunn <dns05018@kakao.com>
2023-05-17 07:08:52 +0900

    modified:   .gitignore 	new file:   create_magic_formula.py 	new file:   load_financial_report.py 	modified:   "\354\213\240 \353\247\210\353\262\225 \352\263\265\354\213\235.ipynb"

---
+++ create_magic_formula.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import os
+import numpy as np
+
+
+
+def main():
+    df = pd.DataFrame(columns=['종목명','시가총액(억 원)','부채율(%)','배당률(%)','PBR','GP/A'])
+    #재무제표 데이터 불러오기
+    report_folder = "/app/Data/Info/Sort By Time/연간/연결"
+    report_df = pd.read_csv(report_folder + "/" + "2022-12-재무제표-연결-연간.csv", encoding='utf-8-sig', index_col=0)
+    report_df.index = report_df.index.astype(str)
+    #zerofill 6
+    report_df.index = report_df.index.str.zfill(6)
+    #포괄손익계산서 데이터 불러오기
+    report_folder = "/app/Data/Info/Sort By Time/연간/연결"
+    report2_df = pd.read_csv(report_folder + "/" + "2022-12-포괄손익계산표-연결-연간.csv", encoding='utf-8-sig', index_col=0)
+    report2_df.index = report2_df.index.astype(str)
+    report2_df.index = report2_df.index.str.zfill(6)
+    #종목 데이터 불러오기
+    stock_folder = "/app/Data/KRX"
+    kospi_df = pd.read_csv(stock_folder + "/" + "kospi_stock_info.csv", encoding='utf-8-sig', index_col=0)
+    kospi_df.index = kospi_df.index.astype(str)
+    kospi_df.index = kospi_df.index.str.zfill(6)
+    kosdaq_df = pd.read_csv(stock_folder + "/" + "kosdaq_stock_info.csv", encoding='utf-8-sig', index_col=0)
+    kosdaq_df.index = kosdaq_df.index.astype(str)
+    kosdaq_df.index = kosdaq_df.index.str.zfill(6)
+
+    for stock_code in report_df.index:
+        try:
+            종목명 = kospi_df.loc[stock_code]['한글명']
+            시가총액 = kospi_df.loc[stock_code]['시가총액']
+            부채비율 = report_df.loc[stock_code]['부채비율']
+            배당수익률 = report_df.loc[stock_code]['배당수익률']
+            PBR = report_df.loc[stock_code]['PBR']
+            매출액 = report2_df.loc[stock_code]['매출액']
+            매출원가 = report2_df.loc[stock_code]['매출원가']
+            매출총이익 = 매출액 - 매출원가
+            자산총계 = report_df.loc[stock_code]['자산총계']
+            GPA = 매출총이익 / 자산총계 * 100
+            df.loc[stock_code] = [종목명, 시가총액, 부채비율, 배당수익률, PBR, GPA]
+        except:
+            try:
+                종목명 = kosdaq_df.loc[stock_code]['한글종목명']
+                시가총액 = kosdaq_df.loc[stock_code]['전일기준 시가총액 (억)']
+                부채비율 = report_df.loc[stock_code]['부채비율']
+                배당수익률 = report_df.loc[stock_code]['배당수익률']
+                PBR = report_df.loc[stock_code]['PBR']
+                매출액 = report2_df.loc[stock_code]['매출액']
+                매출원가 = report2_df.loc[stock_code]['매출원가']
+                매출총이익 = 매출액 - 매출원가
+                자산총계 = report_df.loc[stock_code]['자산총계']
+                GPA = 매출총이익 / 자산총계
+                df.loc[stock_code] = [종목명, 시가총액, 부채비율, 배당수익률, PBR, GPA]
+            except:
+                continue
+            
+    df.sort_values(by=['시가총액(억 원)'], ascending=False)
+    df.dropna(inplace=True)
+    if not os.path.exists("/app/Data/마법공식"):
+        os.makedirs("/app/Data/마법공식")
+        
+    if os.path.exists("/app/Data/마법공식/마법공식.csv"):
+        os.remove("/app/Data/마법공식/마법공식.csv")
+    df.to_csv("/app/Data/마법공식/마법공식.csv", encoding='utf-8-sig')
+        
+            
+   
+    
---
+++ load_financial_report.py
@@ -0,0 +1,146 @@
+import pandas as pd
+import os
+from tqdm import tqdm
+from modules import public_stock
+from natsort import natsorted
+from glob import glob
+
+
+
+#별도 = [매출액,영업이익,영업이익(발표기준),당기순이익,자산총계,부채총계,자본총계,자본금,부채비율,유보율,영업이익률,순이익률,ROA,ROE,EPS(원),BPS(원),DPS(원),PER,PBR,발행주식수,배당수익률]
+재무_별도 = ['매출액','영업이익','영업이익(발표기준)','당기순이익','자산총계','부채총계','자본총계','자본금','부채비율','유보율','영업이익률','순이익률','ROA','ROE','EPS(원)','BPS(원)','DPS(원)','PER','PBR','발행주식수','배당수익률']
+#연결 = [매출액,영업이익,영업이익(발표기준),당기순이익,지배주주순이익,비지배주주순이익,자산총계,부채총계,자본총계,지배주주지분,비지배주주지분,자본금,부채비율,유보율,영업이익률,지배주주순이익률,ROA,ROE,EPS(원),BPS(원),DPS(원),PER,PBR,발행주식수,배당수익률]
+재무_연결 = ['매출액','영업이익','영업이익(발표기준)','당기순이익','지배주주순이익','비지배주주순이익','자산총계','부채총계','자본총계','지배주주지분','비지배주주지분','자본금','부채비율','유보율','영업이익률','지배주주순이익률','ROA','ROE','EPS(원)','BPS(원)','DPS(원)','PER','PBR','발행주식수','배당수익률']
+#포괄_별도 = [매출액,매출원가,매출총이익,판매비와관리비,영업이익,영업이익(발표기준),금융수익,금융원가,기타수익,기타비용,"종속기업,공동지배기업및관계기업관련손익",세전계속사업이익,법인세비용,계속영업이익,중단영업이익,당기순이익]
+포괄_별도 = ['매출액','매출원가','매출총이익','판매비와관리비','영업이익','영업이익(발표기준)','금융수익','금융원가','기타수익','기타비용','종속기업,공동지배기업및관계기업관련손익','세전계속사업이익','법인세비용','계속영업이익','중단영업이익','당기순이익']
+#포괄_연결 = 매출액,매출원가,매출총이익,판매비와관리비,영업이익,영업이익(발표기준),금융수익,금융원가,기타수익,기타비용,"종속기업,공동지배기업및관계기업관련손익",세전계속사업이익,법인세비용,계속영업이익,중단영업이익,당기순이익,지배주주순이익,비지배주주순이익
+포괄_연결 = ['매출액','매출원가','매출총이익','판매비와관리비','영업이익','영업이익(발표기준)','금융수익','금융원가','기타수익','기타비용','종속기업,공동지배기업및관계기업관련손익','세전계속사업이익','법인세비용','계속영업이익','중단영업이익','당기순이익','지배주주순이익','비지배주주순이익']
+
+
+def preprocess_financial_statement_1(df_path):
+    df = pd.read_csv(df_path, skiprows=1)
+    df = df.T
+    df.columns = df.iloc[1]
+    df = df[2:]
+    return df
+
+def preprocess_financial_statement_2(df):
+    df = df.T
+    df.columns = df.iloc[0]
+    df = df[1:-2]
+    df.columns = df.columns.str.replace('계산에 참여한 계정 펼치기', '')
+    return df
+
+def save_df_by_year(path, report_type):
+    csv_cache = {}
+    for i in tqdm(range(len(path))):
+        stock_code = path[i].split("/")[-2]
+        csv_cache[stock_code] = pd.read_csv(path[i], index_col=0)
+        
+    year = csv_cache["005930"].index.tolist()
+
+    df_list = {}
+    for i in year:
+        df_list[i] = pd.DataFrame(columns=report_type)
+
+    for i in tqdm(csv_cache.keys()):
+        for j in year:
+            try:
+                df_list[j].loc[i] = csv_cache[i].loc[j]
+            except:
+                pass
+    return df_list, year
+
+
+def main():
+    error_list = []
+    stock_code_list = natsorted(public_stock.get_stock_list("ALL"))
+    for stock_code in tqdm(stock_code_list):
+        try:
+            if os.path.exists(f"/app/Data/Info/Sort By Stock Code/{stock_code}"):
+                pass
+            else:   
+                os.makedirs(f"/app/Data/Info/Sort By Stock Code/{stock_code}")
+
+            tables = pd.read_html(f"https://comp.fnguide.com/SVO2/ASP/SVD_main.asp?pGB=1&gicode=A{stock_code}&cID=&MenuYn=Y&ReportGB=&NewMenuID=11&stkGb=&strResearchYN=")
+            tables[11].to_csv(f"/app/Data/Info/Sort By Stock Code/{stock_code}/재무제표-연결-연간.csv")
+            preprocess_financial_statement_1(f"/app/Data/Info/Sort By Stock Code/{stock_code}/재무제표-연결-연간.csv").to_csv(f"/app/Data/Info/Sort By Stock Code/{stock_code}/재무제표-연결-연간.csv")
+            tables[12].to_csv(f"/app/Data/Info/Sort By Stock Code/{stock_code}/재무제표-연결-분기.csv")
+            preprocess_financial_statement_1(f"/app/Data/Info/Sort By Stock Code/{stock_code}/재무제표-연결-분기.csv").to_csv(f"/app/Data/Info/Sort By Stock Code/{stock_code}/재무제표-연결-분기.csv")
+            tables[14].to_csv(f"/app/Data/Info/Sort By Stock Code/{stock_code}/재무제표-별도-연간.csv")
+            preprocess_financial_statement_1(f"/app/Data/Info/Sort By Stock Code/{stock_code}/재무제표-별도-연간.csv").to_csv(f"/app/Data/Info/Sort By Stock Code/{stock_code}/재무제표-별도-연간.csv")
+            tables[15].to_csv(f"/app/Data/Info/Sort By Stock Code/{stock_code}/재무제표-별도-분기.csv")
+            preprocess_financial_statement_1(f"/app/Data/Info/Sort By Stock Code/{stock_code}/재무제표-별도-분기.csv").to_csv(f"/app/Data/Info/Sort By Stock Code/{stock_code}/재무제표-별도-분기.csv")
+            tables = pd.read_html(f"https://comp.fnguide.com/SVO2/ASP/SVD_Finance.asp?pGB=1&gicode=A{stock_code}&cID=&MenuYn=Y&ReportGB=&NewMenuID=103&stkGb=701")
+            preprocess_financial_statement_2(tables[0]).to_csv(f"/app/Data/Info/Sort By Stock Code/{stock_code}/포괄손익계산서-연결-연간.csv")
+            preprocess_financial_statement_2(tables[1]).to_csv(f"/app/Data/Info/Sort By Stock Code/{stock_code}/포괄손익계산서-연결-분기.csv")
+            preprocess_financial_statement_2(tables[2]).to_csv(f"/app/Data/Info/Sort By Stock Code/{stock_code}/재무상태표-연결-연간.csv")
+            preprocess_financial_statement_2(tables[3]).to_csv(f"/app/Data/Info/Sort By Stock Code/{stock_code}/재무상태표-연결-분기.csv")
+            
+            tables = pd.read_html(f"https://comp.fnguide.com/SVO2/ASP/SVD_Finance.asp?pGB=1&gicode=A{stock_code}&cID=&MenuYn=Y&ReportGB=B&NewMenuID=103&stkGb=701")
+            preprocess_financial_statement_2(tables[0]).to_csv(f"/app/Data/Info/Sort By Stock Code/{stock_code}/포괄손익계산서-별도-연간.csv")
+            preprocess_financial_statement_2(tables[1]).to_csv(f"/app/Data/Info/Sort By Stock Code/{stock_code}/포괄손익계산서-별도-분기.csv")
+            preprocess_financial_statement_2(tables[2]).to_csv(f"/app/Data/Info/Sort By Stock Code/{stock_code}/재무상태표-별도-연간.csv")
+            preprocess_financial_statement_2(tables[3]).to_csv(f"/app/Data/Info/Sort By Stock Code/{stock_code}/재무상태표-별도-분기.csv")
+            
+        except Exception as e:
+            print(e)
+            error_list.append(stock_code)
+            
+    #error_list를 txt파일로 저장
+    with open("/app/Data/log/재무제표 로드 오류.txt", "w") as f:
+        for error in error_list:
+            f.write(error + "\n")
+            
+    
+    
+    #재무제표_별도_분기
+    path = glob("/app/Data/Info/Sort By Stock Code/**/재무제표-별도-분기.csv")
+    df_list, year = save_df_by_year(path, 재무_별도)
+    for i in year:
+        df_list[i].to_csv("/app/Data/Info/Sort By Time/분기/별도/{}-재무제표-별도-분기.csv".format(i.replace("/","-")))
+        
+    #재무제표_연결_분기
+    path = glob("/app/Data/Info/Sort By Stock Code/**/재무제표-연결-분기.csv")
+    df_list, year = save_df_by_year(path, 재무_연결)
+    for i in year:
+        df_list[i].to_csv("/app/Data/Info/Sort By Time/분기/연결/{}-재무제표-연결-분기.csv".format(i.replace("/","-")))
+        
+    #재무제표_별도_연간
+    path = glob("/app/Data/Info/Sort By Stock Code/**/재무제표-별도-연간.csv")
+    df_list, year = save_df_by_year(path, 재무_별도)
+    for i in year:
+        df_list[i].to_csv("/app/Data/Info/Sort By Time/연간/별도/{}-재무제표-별도-연간.csv".format(i.replace("/","-")))
+        
+    #재무제표_연결_연간
+    path = glob("/app/Data/Info/Sort By Stock Code/**/재무제표-연결-연간.csv")
+    df_list, year = save_df_by_year(path, 재무_연결)
+    for i in year:
+        df_list[i].to_csv("/app/Data/Info/Sort By Time/연간/연결/{}-재무제표-연결-연간.csv".format(i.replace("/","-")))
+        
+    #포괄손익계산서_별도_분기
+    path = glob("/app/Data/Info/Sort By Stock Code/**/포괄손익계산서-별도-분기.csv")
+    df_list, year = save_df_by_year(path, 포괄_별도)
+    for i in year:
+        df_list[i].to_csv("/app/Data/Info/Sort By Time/분기/별도/{}-포괄손익계산서-별도-분기.csv".format(i.replace("/","-")))
+        
+    #포괄손익계산서_연결_분기
+    path = glob("/app/Data/Info/Sort By Stock Code/**/포괄손익계산서-연결-분기.csv")
+    df_list, year = save_df_by_year(path, 포괄_연결)
+    for i in year:
+        df_list[i].to_csv("/app/Data/Info/Sort By Time/분기/연결/{}-포괄손익계산서-연결-분기.csv".format(i.replace("/","-")))
+        
+    #포괄손익계산서_별도_연간
+    path = glob("/app/Data/Info/Sort By Stock Code/**/포괄손익계산서-별도-연간.csv")
+    df_list, year = save_df_by_year(path, 포괄_별도)
+    for i in year:
+        df_list[i].to_csv("/app/Data/Info/Sort By Time/연간/별도/{}-포괄손익계산서-별도-연간.csv".format(i.replace("/","-")))
+        
+    #포괄손익계산서_연결_연간
+    path = glob("/app/Data/Info/Sort By Stock Code/**/포괄손익계산서-연결-연간.csv")
+    df_list, year = save_df_by_year(path, 포괄_연결)
+    for i in year:
+        df_list[i].to_csv("/app/Data/Info/Sort By Time/연간/연결/{}-포괄손익계산서-연결-연간.csv".format(i.replace("/","-")))
+        
+if __name__ == "__main__":
+    main()
